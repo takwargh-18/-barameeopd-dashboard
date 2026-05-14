08f70954d6c91e4926fa6369504654857cf0bf16
takwargh-18 <takwargh@gmail.com>
2026-05-14 17:27:52 +0700

    Delete services directory

--- services/api.js
+++
@@ -1,4 +0,0 @@
-const API_BASE = `http://${window.location.hostname}:9200`;
-
-export const getRootCause = (m, y) =>
-  fetch(`${API_BASE}/api/opd/rootcause?month=${m}&year=${y}`).then(r => r.json());
